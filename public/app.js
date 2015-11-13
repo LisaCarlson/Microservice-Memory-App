b2a7a9ef14eec6e9ddbe6be3454a638e8499f82f
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-12 23:25:35 -0700

    style changes

--- public/app.js
+++ public/app.js
@@ -1,39 +1,12 @@
 angular.module('MyApp', ['ngRoute'])
   .controller('HomeController', ['$scope', '$http', function ($scope, $http) {
-    // $http.get('https://still-tundra-8387.herokuapp.com/messages').then(function(response){
-    //   $scope.messages = response.data;
+    // $http.get('http://g12-lisa-carlson-memories.cfapps.io/api/v1/memories').then(function(response){
+    //   $scope.memories = response.data;
+    //   console.log($scope.memories)
     // });
-    // $scope.submit = function () {
-    //   var dataObj = {
-    //     name : $scope.name,
-    //     content : $scope.message,
-    //   };  
-    //   $http.post('https://still-tundra-8387.herokuapp.com/messages', {message: dataObj}).then(function(response){
-    //       console.log(response.data)
-    //   }, function(){
-    //       console.log('error');
-    //   });
-    //   $scope.name='';
-    //   $scope.message='';
-    // }
   }])
   .controller('YearController', ['$scope', '$http', function ($scope, $http) {
-    // $http.get('https://still-tundra-8387.herokuapp.com/messages').then(function(response){
-    //   $scope.messages = response.data;
-    // });
-    // $scope.submit = function () {
-    //   var dataObj = {
-    //     name : $scope.name,
-    //     content : $scope.message,
-    //   };  
-    //   $http.post('https://still-tundra-8387.herokuapp.com/messages', {message: dataObj}).then(function(response){
-    //       console.log(response.data)
-    //   }, function(){
-    //       console.log('error');
-    //   });
-    //   $scope.name='';
-    //   $scope.message='';
-    // }
+    
   }])
   .config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
